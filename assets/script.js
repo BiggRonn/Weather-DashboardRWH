9de497cdccac7e3331421c775648eb564c8ea4f2
Wes Howard <BigRonald42@hotmail.com>
2021-03-09 21:18:23 -0700

    getting started, added search bar

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var api_key = "7e4dc32eeeff2ca5b970045a0cb819aa";
+
+//var base_url =  + api_key;
+
+const currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=" + api_key;
+
+//const forcastWeatherUrl =  + api_key;
